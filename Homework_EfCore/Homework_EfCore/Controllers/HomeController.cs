72b10acaa086b644e8f63b0b1cc1cc003d43dbb7
Konstantin.Borovik <konstantin.borovik@fortegrp.com>
2022-11-07 10:50:12 +0300

    - init

---
+++ Homework_EfCore/Homework_EfCore/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Homework_EfCore.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
